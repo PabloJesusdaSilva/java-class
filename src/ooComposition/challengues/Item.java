6b3fa947b2c60983057ec2c7de2baed6c45c4fe3
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2024-05-23 20:23:50 -0300

    :feat: first OO challlengue

---
+++ src/ooComposition/challengues/Item.java
@@ -0,0 +1,15 @@
+package ooComposition.challengues;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item {
+
+    final Product product;
+    final int amound;
+
+    Item(Product product, int amound) {
+        this.product = product;
+        this.amound = amound;
+    }
+}
---
+++ src/ooComposition/challengues/Product.java
@@ -0,0 +1,12 @@
+package ooComposition.challengues;
+
+public class Product {
+
+    String name;
+    double price;
+
+    Product (String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}

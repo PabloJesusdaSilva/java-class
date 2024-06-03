7ed099a75a4ca33fdc016429764c1e0938ee40a8
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2024-06-03 20:01:32 -0300

    :feat: encapsulation class

---
+++ src/ooComposition/encapsulamento/casaA/Paulo.java
@@ -0,0 +1,13 @@
+package ooComposition.encapsulamento.casaA;
+
+public class Paulo {
+
+    Ana wife = new Ana();
+
+    void test() {
+        //System.out.println(wife.variableOne);
+        System.out.println(wife.variableTwo);
+        System.out.println(wife.variableThree);
+        System.out.println(wife.variableFour);
+    }
+}

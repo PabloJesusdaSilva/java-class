ff4ebf2a3f6643b44cdcbc00709c60e7aab3bb68
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2024-06-04 19:29:05 -0300

    :feat: encapsulation class 2

--- src/ooComposition/encapsulamento/casaA/Paulo.java
+++ src/ooComposition/encapsulamento/casaA/Paulo.java
@@ -1,11 +1,11 @@
 package ooComposition.encapsulamento.casaA;
 
 public class Paulo {
-
-    Ana wife = new Ana();
-
+    
     void test() {
-        //System.out.println(wife.variableOne);
+        Ana wife = new Ana();
+
+        // System.out.println(wife.variableOne);
         System.out.println(wife.variableTwo);
         System.out.println(wife.variableThree);
         System.out.println(wife.variableFour);
---
+++ src/ooComposition/encapsulamento/casaB/Pedro.java
@@ -0,0 +1,13 @@
+package ooComposition.encapsulamento.casaB;
+
+import ooComposition.encapsulamento.casaA.Ana;
+
+public class Pedro extends Ana {
+
+    void test() {
+        // System.out.println(mother.variableOne);
+        // System.out.println(mother.variableTwo);
+        System.out.println(variableThree);
+        System.out.println(variableFour);
+    }
+}

0e4271c023abc519b5c2fbfa26e53c08ebcd5bf0
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2024-06-04 20:09:33 -0300

    :feat: getter and setter class

---
+++ src/ooComposition/encapsulamento/PersonTest.java
@@ -0,0 +1,12 @@
+package ooComposition.encapsulamento;
+
+public class PersonTest {
+
+    public static void main(String[] args) {
+        Person person = new Person("Pablo", "Jesus", -19);
+
+        System.out.println(); // toString
+        System.out.println(person); // toString
+        System.out.println(person.fullName());
+    }
+}
